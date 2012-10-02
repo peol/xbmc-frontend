f7a4a5e89a052d74c88078900040705f75222c0d
Andrée Hansson <peolanha@gmail.com>
2012-10-02 19:48:36 +0200

    Optimize build, exclude a bunch of stuff from `components/`.

--- build/buildconfig.js
+++ build/buildconfig.js
@@ -3,6 +3,7 @@
 	baseUrl: 'js/',
 	dir: '../target',
 	mainConfigFile: '../js/main.js',
+	fileExclusionRegExp: /^examples|test|demo|vendor|build|dist|^r\.js$|.+?(\.|-)min\.js$/i,
 	modules: [{
 		name: "main"
 	}]
